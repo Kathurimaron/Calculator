6a6cde2ca4c9498a36bbf73d2df626912bff445e
Kathurimaron <92383468+Kathurimaron@users.noreply.github.com>
2021-10-12 17:39:29 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,3 @@
-/*
-Calculator program that cimputes 2 numbers and return 
-sum, diff, product, qutient and modulus
-
-computation:
-sum = num1 + num2;
-   -diff = num1 - num2;
-   -pro = num1 * num2;
-   -quot = num1 / num2;
-   -mod = num1 % num2;
-*/
 #include <stdio.h>
 #include<stdlib.h>
 
@@ -18,18 +7,17 @@ int main(){
     int sum, diff, pro, mod;
     float quot;
 
-    //assignment
+    //assigning values
     num1 = 200;
     num2 = 56;
 
-    //calculation
+    //computation
     sum = num1 + num2;
     diff = num1 - num2;
     pro = num1 * num2;
     quot = (float)num1 / num2;
     mod = num1 % num2;
 
-    //output
     printf("Sum: %i + %i = %i \n",num1, num2, sum);
     printf("Diffrence: %i - %i = %i \n",num1, num2, diff);
     printf("Product: %i * %i = %i \n",num1, num2, pro);
